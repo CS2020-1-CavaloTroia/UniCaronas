79efc420f56f1302d4a7b7f79a5a7ad36277c85c
Fernando Severino Almeida <fernandosevjipa@gmail.com>
2020-10-26 17:21:51 -0400

    test

--- server/UniCaronas/UniCaronasServer/settings.py
+++ server/UniCaronas/UniCaronasServer/settings.py
@@ -6,7 +6,7 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-# reading .env file
+# reading .ENV file
 environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
