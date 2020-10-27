0d20a26207344b4dd1406ed91c03615b6a81c57d
Fernando Severino Almeida <fernandosevjipa@gmail.com>
2020-10-26 20:52:25 -0400

    update requirements.txt

--- server/UniCaronas/UniCaronasServer/settings.py
+++ server/UniCaronas/UniCaronasServer/settings.py
@@ -2,12 +2,22 @@ import os
 from pathlib import Path
 import environ
 
+try:
+    environ.Env.read_env()
+except:
+    print('Create an .env!')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG')
+
+DBNAME = os.getenv('DBNAME')
+DBHOST = os.getenv('DBHOST')
+DBUSERNAME = os.getenv('DBUSERNAME')
+DBPASS = os.getenv('DBPASS')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-# reading .ENV file
-environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -15,8 +25,6 @@ environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ieh2(x%8ey)6i8(j#x=a#7udw@_u&nr8mgc&*a$v#3c*odk2yf'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
 
 X_FRAME_OPTIONS = 'ALLOWALL'
 
@@ -79,33 +87,22 @@ WSGI_APPLICATION = 'UniCaronasServer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASE = {
-    'ENGINE': 'djongo',
-    "CLIENT": {
-        "name": env('DBNAME'),
-        "host": env('DBHOST'),
-        "username": env('DBUSERNAME'),
-        "password": env('DBPASS'),
-        "authMechanism": "SCRAM-SHA-1",
-    },
-} if DEBUG else {
-    'ENGINE': 'djongo',
-    "CLIENT": {
-        "name": env('STAGE_DBNAME'),
-        "host": env('STAGE_DBHOST'),
-        "username": env('STAGE_DBUSERNAME'),
-        "password": env('STAGE_DBPASS'),
-        "authMechanism": "SCRAM-SHA-1",
-    },
-}
-
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
 
-    'default': DATABASE
+    'default': {
+        'ENGINE': 'djongo',
+        "CLIENT": {
+            "name": DBNAME,
+            "host": DBHOST,
+            "username": DBUSERNAME,
+            "password": DBPASS,
+            "authMechanism": "SCRAM-SHA-1",
+        },
+    }
 }
 
 # Password validation
@@ -140,6 +137,9 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
